24bfd5ef9b1efaa24a7caae922172181c39e0a46
Afope <afopematilukuro@gmail.com>
2022-09-30 15:39:45 -0400

    Challenge Completed

--- script/script.js
+++ script/script.js
@@ -1,1 +1,45 @@
 "use strict";
+
+const nav = document.querySelector(".nav");
+const mobileMenu = document.getElementById("burger__check");
+const header = document.querySelector(".header");
+const footer = document.querySelector(".footer");
+
+mobileMenu.addEventListener("click", function (e) {
+  if (e.target.checked) {
+    nav.classList.add("menu-reveal");
+  } else {
+    hideMobileMenu();
+  }
+});
+
+nav.addEventListener("click", scrolling.bind(".nav__links"));
+footer.addEventListener("click", scrolling.bind(".footer__link"));
+
+function hideMobileMenu() {
+  nav.classList.remove("menu-reveal");
+  mobileMenu.checked = false;
+}
+
+function scrolling(e) {
+  const link = e.target.closest(this);
+  if (!link) return;
+  e.preventDefault();
+  document
+    .querySelector(`${link.getAttribute("href")}`)
+    .scrollIntoView({ behavior: "smooth" });
+
+  if (this == ".nav__links") {
+    hideMobileMenu();
+  }
+}
+
+const windowWidthResize = new ResizeObserver(function (entries) {
+  entries.forEach(function (entry) {
+    if (entry.contentRect.width >= 782) {
+      hideMobileMenu();
+    }
+  });
+});
+
+windowWidthResize.observe(header);
